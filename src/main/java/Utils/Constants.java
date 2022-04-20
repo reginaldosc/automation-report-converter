e3ba84a3e37ea6abf19fd1acac0c0ab770ffc9b6
Reginaldo Gonçalves <reginaldo.goncalves@bringglobal.com>
2022-04-20 11:02:02 -0300

    Change for CGD

--- src/main/java/Utils/Constants.java
+++ src/main/java/Utils/Constants.java
@@ -3,12 +3,15 @@ package Utils;
 
 public class Constants {
     public final static String cucumberReportPath   = "/Volumes/HP P500/cgd-mobile-automation/cgd-mobile-automation/target/cucumber-report/";
-    public final static String elkReportPath        = "/Volumes/HP P500/Cucumber-Reports/";
+    public final static String elkReportPath        = "/Volumes/HP P500/cgd-mobile-automation/cgd-mobile-automation/target/CGD-report/";
+    //public final static String elkReportPath        = "/Volumes/HP P500/Cucumber-Reports/";
     public final static String jsonExtension        = ".json";
     public final static String logExtension         = ".log";
     public final static String iosReportName        = "ios-cucumber";
     public final static String androidReportName    = "android-cucumber";
-    public final static String outDirectoryReports  = "reports/out/";
-    public final static String inDirectoryReports   = "reports/in/";
+    public final static String outDirectoryReports  = "/Volumes/HP P500/cgd-mobile-automation/cgd-mobile-automation/target/CGD-report/out/";
+    //public final static String outDirectoryReports  = "reports/out/";
+    public final static String inDirectoryReports   = "/Volumes/HP P500/cgd-mobile-automation/cgd-mobile-automation/target/CGD-report/in/";
+    //public final static String inDirectoryReports   = "reports/in/";
 
 }
